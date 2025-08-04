12fbb2037d60ad58b8430d02028302f8bbf9c3ec
¨AntonioBDev¨ <basi.dev9@gmail.com>
2025-08-03 22:13:07 -0600

    ¨Config¨

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
 // variables 
 // const btnLanguage = document.querySelectorAll("select option [data-language]");
+const userLanguage = localStorage.getItem('language') || 'es';
+let translations = {}
 const btnSelectLanguage = document.querySelector('#languages');
-
 const textToChange = document.querySelectorAll("[data-section]");
+let idioma = "";
 document.addEventListener("DOMContentLoaded",()=> {
+    
+    //Cargar traducciones
+    async function loadTranslations(lang) {
+        try {
+            const response = await fetch(`../languages/${lang}.json`);      
+            if(!response.ok){
+                throw new Error(`Error al cargar el archivo de idioma: ${response.statusText}`);
+            }
+            translations = await response.json();
+            console.log(translations);
+            // applyTranslations();
+            localStorage.setItem('language', lang);
+            document.documentElement.lang = lang;
+
+        } catch (error) {
+            console.log("No se puede cargar las traducciones del json");
+        }
+    }
 
     //Valor del elemento OPTION del SELECT
      btnSelectLanguage.addEventListener('change', (e)=>{
-        const language = e.target.value;
-        traducir(language);
+        const idioma = e.target.value;
+        traducir(idioma);
+        loadTranslations(idioma);
     })
   
     function traducir(language){
