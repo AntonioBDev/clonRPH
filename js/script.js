c743e70764d221be45ca370e52e32e1d6d8fd33f
AntonioBDev <basi.dev9@gail.com>
2025-08-07 14:03:40 -0700

    Limpieza de codigo js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 // variables
 const userLanguage = localStorage.getItem("language") || "es";
-let translations = {};
 const btnSelectLanguage = document.querySelector("#languages");
 const textToChange = document.querySelectorAll("[data-section]");
+let translations = {};
+
+//EventListener
 document.addEventListener("DOMContentLoaded", () => {
   loadTranslations(userLanguage);
-  
+
   // Valor del elemento OPTION del SELECT
   btnSelectLanguage.addEventListener("change", (e) => {
     const lang = e.target.value;
     loadTranslations(lang);
   });
 });
-
 
 //Cargar traducciones
 async function loadTranslations(lang) {
@@ -33,28 +34,29 @@ async function loadTranslations(lang) {
   }
 }
 
+//Aplicar traducción
 function applyTranslations() {
-     textToChange.forEach((element) => {
-      const section = element.dataset.section;
-      const value = element.dataset.value;
-      let translatedText = translations;
+  textToChange.forEach((element) => {
+    const section = element.dataset.section;
+    const value = element.dataset.value;
+    let translatedText = translations;
 
-      if(translatedText !== undefined){
-           // Si es un input o textarea, actualiza el atributo placeholder
-            if (element.tagName === 'INPUT'){
-              if(element.type === "submit"){
-                element.setAttribute('value', translations[section][value])
-              }else{
-                element.setAttribute('placeholder', translations[section][value]);
-              }
-            }else if(element.tagName === 'TEXTAREA'){
-              element.setAttribute('placeholder', translations[section][value]);
-            }else {
-                // Para otros elementos, actualiza el innerHTML
-                element.innerHTML = translations[section][value];
-            }
+    if (translatedText !== undefined) {
+      // Si es un input o textarea, actualiza el atributo placeholder o value
+      if (element.tagName === "INPUT") {
+        if (element.type === "submit") {
+          element.setAttribute("value", translations[section][value]);
+        } else {
+          element.setAttribute("placeholder", translations[section][value]);
         }
-      });
+      } else if (element.tagName === "TEXTAREA") {
+        element.setAttribute("placeholder", translations[section][value]);
+      } else {
+        // Para otros elementos, actualiza el innerHTML
+        element.innerHTML = translations[section][value];
+      }
+    }
+  });
 }
 
 function updateLanguageSelect(currentLang) {
